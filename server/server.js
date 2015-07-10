c69da6ae5c4fafc3ceb265efc13572e051d24c62
Jesse Abramson <jesse@influitive.com>
2015-07-10 19:00:53 -0400

    -m

--- server/server.js
+++ server/server.js
@@ -41,135 +41,134 @@ Meteor.startup(function () {
 
   	}
 
-    turn();
-
-});
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function current_world() {
-  return Worlds.findOne({ live: true })._id;
-}
-
-//................Collection Pubs
-
-Meteor.publish('hexes', function() {
-    return Hexes.find({ world: current_world() });
-});
-
-Meteor.publish("allUserData", function () {
-    return Meteor.users.find({}, {fields: { 'username': 1, 'owned': 1, 'colour': 1 }});
-});
-
-Meteor.publish("wealth", function () {
-    return Meteor.users.find(Meteor.userId, {fields: { 'wealth': 1 }});
-
-});
-Meteor.publish("timer", function () {
-    return Timer.find({ world: current_world() });
-});
-
-//................Methods
-
-Meteor.methods({
-  buyHex : function(hex){
-    current_user = Meteor.users.find({_id: this.userId}).fetch();
-    if (!current_user.hexes) {
-      Hexes.update(hex._id, {$set: {owner: this.userId}});
-      Meteor.users.update( { _id: Meteor.userId() }, { $set: {owned: hex._id} });
-    }  
-  },
-  updateWall : function(hexes){
-    hexes.forEach(function (id) {
-      hex = Hexes.findOne({ _id: id });
-      Hexes.update(hex, { $set: {walls: hex.look()} });  
-    });
-  }
-});
-
-//...............Account Hooks
-
-Accounts.onCreateUser( function (options, user) {
-    user.wealth = 100;
-    user.population = 100;
-    user.resources.wood = 100;
-    user.resources.ore = 100;
-    user.resources.glass = 100;
-    user.colour = getRandomColor();
-    return user;
-});
-
-Meteor.users.deny({  
-  update: function() {
-    return true;
-  }
-});
-
-//................Wall Formatting
-
-Hexes.before.update(function (userId, doc, fieldNames, modifier, options) {
-
-}, {fetchPrevious: false});
-
-Hexes.after.update(function (userId, doc, fieldNames, modifier, options) {
-  if (fieldNames.indexOf('owner') >= 0) {
-    hex = Hexes.findOne({_id: doc._id});
-    Hexes.update(hex, { $set: {walls: hex.look()} });
-    var neighbors = [];
-    for (i=0;i<6;i++) {
-      if (!_.isUndefined(hex.look(i)) && hex.look(i).owner === hex.owner) {
-        i >= 3 ? j = (i - 3) : j = (i + 3);
-        neighbors.push(hex.look(i)._id);
+  function getRandomColor() {
+      var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+      for (var i = 0; i < 6; i++ ) {
+          color += letters[Math.floor(Math.random() * 16)];
       }
+      return color;
+  }
+
+  // function current_world() {
+  //   return Worlds.findOne({ live: true })._id;
+  // }
+
+  //................Collection Pubs
+
+  Meteor.publish('hexes', function() {
+      return Hexes.find({ world: Worlds.findOne({ live: true })._id });
+  });
+
+  Meteor.publish("allUserData", function () {
+      return Meteor.users.find({}, {fields: { 'username': 1, 'owned': 1, 'colour': 1 }});
+  });
+
+  Meteor.publish("wealth", function () {
+      return Meteor.users.find(Meteor.userId, {fields: { 'wealth': 1 }});
+
+  });
+  Meteor.publish("timer", function () {
+      return Timer.find({ world: Worlds.findOne({ live: true })._id });
+  });
+
+  //................Methods
+
+  Meteor.methods({
+    buyHex : function(hex){
+      current_user = Meteor.users.find({_id: this.userId}).fetch();
+      if (!current_user.hexes) {
+        Hexes.update(hex._id, {$set: {owner: this.userId}});
+        Meteor.users.update( { _id: Meteor.userId() }, { $set: {owned: hex._id} });
+      }  
+    },
+    updateWall : function(hexes){
+      hexes.forEach(function (id) {
+        hex = Hexes.findOne({ _id: id });
+        Hexes.update(hex, { $set: {walls: hex.look()} });  
+      });
     }
-    Meteor.call('updateWall', neighbors, function (error, result) {});
-  }
-}, {fetchPrevious: false});
+  });
 
-//................Game Timer
+  //...............Account Hooks
 
-var startTime, countAmt, interval, timer;
-timer = Timer.findOne({ world: current_world() });
+  Accounts.onCreateUser( function (options, user) {
+      user.wealth = 100;
+      user.population = 100;
+      user.resources = {};
+      user.resources.wood = 100;
+      user.resources.ore = 100;
+      user.resources.glass = 100;
+      user.colour = getRandomColor();
+      return user;
+  });
 
-Meteor.setInterval(function () {
-    turn();
-}, turnTime);
+  Meteor.users.deny({  
+    update: function() {
+      return true;
+    }
+  });
 
-function turn() {
-  Meteor._debug("Turn");
-  startTimer(turnTime);
-}
+  //................Wall Formatting
 
-function now() {
-  return ((new Date()).getTime()+2);
-}
+  Hexes.before.update(function (userId, doc, fieldNames, modifier, options) {
 
-function tick() {
-  var elapsed = now() - startTime;
-  Meteor._debug(elapsed);
-  var cnt = countAmt - elapsed;
-  Meteor._debug(cnt);
-  Meteor._debug("Tick");
-  if (cnt > 0) {''
-    Timer.update(timer._id, { $set: { remaining: cnt }});
-  } else {
-    Timer.update(timer, { $set: { remaining: 0 }});
+  }, {fetchPrevious: false});
+
+  Hexes.after.update(function (userId, doc, fieldNames, modifier, options) {
+    if (fieldNames.indexOf('owner') >= 0) {
+      hex = Hexes.findOne({_id: doc._id});
+      Hexes.update(hex, { $set: {walls: hex.look()} });
+      var neighbors = [];
+      for (i=0;i<6;i++) {
+        if (!_.isUndefined(hex.look(i)) && hex.look(i).owner === hex.owner) {
+          i >= 3 ? j = (i - 3) : j = (i + 3);
+          neighbors.push(hex.look(i)._id);
+        }
+      }
+      Meteor.call('updateWall', neighbors, function (error, result) {});
+    }
+  }, {fetchPrevious: false});
+
+  //................Game Timer
+
+  var startTime, countAmt, interval, timer;
+  timer = Timer.findOne({ world: Worlds.findOne({ live: true })._id })._id;
+
+  Meteor.setInterval(function () {
+      turn();
+  }, turnTime);
+
+  function turn() {
+    startTimer(turnTime);
     Timer.update(timer, { $inc: { turns: 1 }});
-    clearInterval(interval);
+    Meteor._debug("Turn " + Timer.findOne(timer).turns);
   }
-}
 
-function startTimer(millisecs) {
-  clearInterval(interval);
-  Timer.update(timer._id, { $set: { remaining: millisecs }});
-  countAmt = millisecs;
-  startTime = now();
-  interval = Meteor.setInterval(tick, 1000);  
-}
+  function now() {
+    return ((new Date()).getTime()+2);
+  }
+
+  function tick() {
+    var elapsed = now() - startTime;
+    var cnt = countAmt - elapsed;
+    // Meteor._debug("Tick");
+    if (cnt > 0) {''
+      Timer.update(timer, { $set: { remaining: cnt }});
+    } else {
+      Timer.update(timer, { $set: { remaining: 0 }});
+      clearInterval(interval);
+    }
+  }
+
+  function startTimer(millisecs) {
+    clearInterval(interval);
+    Timer.update(timer, { $set: { remaining: millisecs }});
+    countAmt = millisecs;
+    startTime = now();
+    interval = Meteor.setInterval(tick, 1000);  
+  }
+
+  turn();
+
+});
